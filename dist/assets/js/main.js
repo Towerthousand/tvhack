e5642b7c1e0f1ddf8d49351763467cd7576ca7f0
Daniel Torramilans <daniel@gamemotiv.com>
2015-09-12 02:21:16 -0400

    Stuff

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,10 @@
+//If we are a stb, set the resolution
+if (!!navigator.setResolution) {
+  navigator.setResolution(1280, 720);
+}
+
+//Disable websecutiry to bypass CORS issues if any.
+if (!!navigator.setWebSecurityEnabled){
+  navigator.setWebSecurityEnabled(false);
+}
 

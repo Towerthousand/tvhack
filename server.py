5fe463037aff4ad1cc18fe50beee704b894b73f4
Miquel Llobet <mllobet.cm@gmail.com>
2015-09-12 11:35:56 -0400

    Added template to the server

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
-#!/usr/env/bin python3
+#!/usr/bin/env python3
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == "__main__":
-    app.run(debug==True)
+    app.run(debug=True)
